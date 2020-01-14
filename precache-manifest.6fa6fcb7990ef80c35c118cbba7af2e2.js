6988f4c810134bafa21baebf3c74f5c570e577c8
Victor Vasquez <vvac15@gmail.com>
2020-01-14 10:42:07 -0300

    Updates

---
+++ precache-manifest.6fa6fcb7990ef80c35c118cbba7af2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67c26f6a9e5e90b1c54b39cdf41c407",
+    "url": "/comments-thread/index.html"
+  },
+  {
+    "revision": "27c2494b97ee73aa2324",
+    "url": "/comments-thread/static/css/main.ba91e5df.chunk.css"
+  },
+  {
+    "revision": "20191d6af5ef1e417ef9",
+    "url": "/comments-thread/static/js/2.16851ec1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/comments-thread/static/js/2.16851ec1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27c2494b97ee73aa2324",
+    "url": "/comments-thread/static/js/main.c21c9b53.chunk.js"
+  },
+  {
+    "revision": "b590d3d4b6388d7eaaee",
+    "url": "/comments-thread/static/js/runtime-main.87d43a1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comments-thread/static/media/logo.5d5d9eef.svg"
+  }
+]);
